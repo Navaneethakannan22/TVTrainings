3653293ec0a5deda4d2690501559fb6fd3389f7b
Navaneeth <Navaneethakannan.Palanisamy@expleogroup.com>
2021-07-07 01:14:50 +0530

    Fruit Segregation code is added

--- TV/src/com/tv/trainings/FruitsSection.java
+++ TV/src/com/tv/trainings/FruitsSection.java
@@ -1,8 +1,14 @@
 package com.tv.trainings;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Fruit {
 	private String color;
 	private int pricePerKg;
+
+	Fruit() {
+	}
 
 	Fruit(String color, int pricePerKg) {
 		this.color = color;
@@ -19,31 +25,73 @@ class Fruit {
 }
 
 class Apple extends Fruit {
-	static int count;
+	// static int count;
+	ArrayList<Object> apple = new ArrayList<>();
+
+	public Apple() {
+	}
 
 	Apple(String color, int pricePerKg) {
 		super(color, pricePerKg);
-		count++;
 	}
 
+	public void segregateAppleFruit(List<Fruit> fruits) {
+		for (Fruit fruit : fruits) {
+			if (fruit instanceof Apple) {
+				apple.add(fruit);
+			}
+		}
+	}
+
+	public int getAppleCount() {
+		return apple.size();
+	}
 }
 
 class Banana extends Fruit {
-	static int count;
+	// static int count;
+	ArrayList<Object> banana = new ArrayList<>();
+
+	public Banana() {
+	}
 
 	Banana(String color, int pricePerKg) {
 		super(color, pricePerKg);
-		count++;
 	}
-	
+
+	public void segregateBananaFruit(List<Fruit> fruits) {
+		for (Fruit fruit : fruits) {
+			if (fruit instanceof Banana) {
+				banana.add(fruit);
+			}
+		}
+	}
+
+	public int getBananaCount() {
+		return banana.size();
+	}
 }
 
 class Orange extends Fruit {
-	static int count;
+	// static int count;
+	ArrayList<Object> orange = new ArrayList<>();
+
+	public Orange() {
+	}
 
 	Orange(String color, int pricePerKg) {
 		super(color, pricePerKg);
-		count++;
 	}
-	
+
+	public void segregateOrangeFruit(List<Fruit> fruits) {
+		for (Fruit fruit : fruits) {
+			if (fruit instanceof Orange) {
+				orange.add(fruit);
+			}
+		}
+	}
+
+	public int getOrangeCount() {
+		return orange.size();
+	}
 }
